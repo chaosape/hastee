8fdb65e65ebc171938ee881c7359673fb02e0817
Matthieu Wipliez <matthieu.wipliez@insa-rennes.fr>
2011-07-13 11:56:50 +0200

    (bug correction: memory leak) by changing the map in call graph to a map from group to weak references.

--- plugins/net.sf.hastee/src/net/sf/hastee/scoping/CallGraph.java
+++ plugins/net.sf.hastee/src/net/sf/hastee/scoping/CallGraph.java
@@ -19,6 +19,7 @@
  */
 package net.sf.hastee.scoping;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
@@ -48,7 +49,7 @@ import org.eclipse.xtext.EcoreUtil2;
  */
 public class CallGraph {
 
-	private static final Map<Group, CallGraph> map = new WeakHashMap<Group, CallGraph>();
+	private static final Map<Group, WeakReference<CallGraph>> map = new WeakHashMap<Group, WeakReference<CallGraph>>();
 
 	/**
 	 * Returns the call graph associated with the given group. The call graph is
@@ -62,12 +63,19 @@ public class CallGraph {
 		// at most one access on the map
 		// because Xtext spawns one build thread + one validation thread
 		synchronized (map) {
-			CallGraph cg = map.get(group);
-			if (cg == null) {
-				cg = new CallGraph();
-				cg.build(group);
-				map.put(group, cg);
+			WeakReference<CallGraph> ref = map.get(group);
+			if (ref != null) {
+				CallGraph cg = ref.get();
+				if (cg != null) {
+					return cg;
+				}
 			}
+
+			CallGraph cg = new CallGraph();
+			cg.build(group);
+			ref = new WeakReference<CallGraph>(cg);
+			map.put(group, ref);
+
 			return cg;
 		}
 	}
