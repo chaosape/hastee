174fa3528dbb041f574474b5239db806c3a1f1ea
Matthieu Wipliez <matthieu.wipliez@synflow.com>
2012-01-25 14:40:15 +0100

    (core) added check of number of arguments in template references. Disabled resolution of implicit attributes (buggy).

--- plugins/net.sf.hastee/src/net/sf/hastee/scoping/STScopeProvider.java
+++ plugins/net.sf.hastee/src/net/sf/hastee/scoping/STScopeProvider.java
@@ -21,19 +21,14 @@ package net.sf.hastee.scoping;
 
 import net.sf.hastee.st.Declaration;
 import net.sf.hastee.st.ExprReference;
-import net.sf.hastee.st.Group;
 import net.sf.hastee.st.TemplateAnonymous;
 import net.sf.hastee.st.TemplateDeclaration;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
-import org.eclipse.xtext.EcoreUtil2;
-import org.eclipse.xtext.scoping.IGlobalScopeProvider;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.Scopes;
 import org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider;
-
-import com.google.inject.Inject;
 
 /**
  * This class defines a scope provider for ST.
@@ -41,9 +36,6 @@ import com.google.inject.Inject;
  * @author Matthieu Wipliez
  */
 public class STScopeProvider extends AbstractDeclarativeScopeProvider {
-
-	@Inject
-	private IGlobalScopeProvider provider;
 
 	public IScope scope_Arg_attribute(ExprReference expr, EReference reference) {
 		Declaration declaration = expr.getTarget();
@@ -65,11 +57,12 @@ public class STScopeProvider extends AbstractDeclarativeScopeProvider {
 
 	public IScope scope_ExprAttribute_attribute(TemplateDeclaration decl,
 			EReference reference) {
-		Group group = EcoreUtil2.getContainerOfType(decl, Group.class);
-		CallGraph icg = CallGraph.getCallGraph(group, provider);
-		EObject caller = icg.getCaller(decl);
-		IScope outer = (caller == null) ? delegateGetScope(decl, reference)
-				: getScope(caller, reference);
+		// Group group = EcoreUtil2.getContainerOfType(decl, Group.class);
+		// CallGraph icg = CallGraph.getCallGraph(group, provider);
+		// EObject caller = icg.getCaller(decl);
+		// IScope outer = (caller == null) ? delegateGetScope(decl, reference)
+		// : getScope(caller, reference);
+		IScope outer = delegateGetScope(decl, reference);
 		IScope scope = Scopes.scopeFor(decl.getAttributes(), outer);
 		return scope;
 	}
--- plugins/net.sf.hastee/src/net/sf/hastee/validation/STJavaValidator.java
+++ plugins/net.sf.hastee/src/net/sf/hastee/validation/STJavaValidator.java
@@ -1,12 +1,32 @@
 package net.sf.hastee.validation;
 
+import net.sf.hastee.st.Declaration;
+import net.sf.hastee.st.ExprReference;
+import net.sf.hastee.st.ExprTemplateArgs;
+import net.sf.hastee.st.StPackage;
+import net.sf.hastee.st.TemplateDeclaration;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.validation.Check;
+
 public class STJavaValidator extends AbstractSTJavaValidator {
 
-	// @Check
-	// public void checkGreetingStartsWithCapital(Greeting greeting) {
-	// if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-	// warning("Name should start with a capital", MyDslPackage.GREETING__NAME);
-	// }
-	// }
+	@Check
+	public void checkTemplateReference(ExprReference ref) {
+		Declaration decl = ref.getTarget();
+		EObject contents = decl.getContents();
+		if (contents instanceof TemplateDeclaration) {
+			TemplateDeclaration tmplDecl = (TemplateDeclaration) contents;
+			int expected = tmplDecl.getAttributes().size();
+
+			ExprTemplateArgs args = ref.getArgs();
+			int actual = (args == null) ? 0 : args.getArgs().size();
+			if (expected != actual) {
+				error("Number of arguments mismatch: expected " + expected
+						+ " arguments, got " + actual + " arguments", ref,
+						StPackage.Literals.EXPR_REFERENCE__TARGET, -1);
+			}
+		}
+	}
 
 }
